700f6fd9db2d1ba3f19fc83adfab96ad2b266ec6
Sinan Ozel <4183117+sinan-ozel@users.noreply.github.com>
2026-06-04 21:29:53 -0400

    Release kubyterhub-ds:26.05

--- kubyterhub-ds/start-jupyterhub.sh
+++ kubyterhub-ds/start-jupyterhub.sh
@@ -6,6 +6,9 @@ echo "Starting JupyterHub with dynamic user creation..."
 # Create users from userlist.txt
 /usr/local/bin/create-users.sh
 
+# Clean up stale PID files left by previous runs
+rm -f /home/jovyan/work/jupyterhub-proxy.pid /home/jovyan/work/jupyterhub.pid
+
 # Start JupyterHub
 echo "Starting JupyterHub..."
 exec jupyterhub --ip=0.0.0.0 --port=8000 --config=/etc/jupyterhub/jupyterhub_config.py "$@"
